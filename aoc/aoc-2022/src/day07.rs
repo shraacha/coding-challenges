da8f61fc40848bea4e97cc0b5eb8bca998592b21
aracharla <aracharla@blackberry.com>
2022-12-07 08:54:14 -0500

    7 init

---
+++ aoc/aoc-2022/src/day07.rs
@@ -0,0 +1,15 @@
+pub fn a () {
+    let input = include_str!("../inputs/dayXX.txt");
+
+    let output = 0;
+
+    println!("dayXX a: {output}");
+}
+
+pub fn b () {
+    let input = include_str!("../inputs/dayXX.txt");
+
+    let output = 0;
+
+    println!("dayXX b: {output}");
+}
